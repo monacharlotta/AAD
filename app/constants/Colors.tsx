f7ed5eb6e37cacf02914608a8bce65bddb84f1ae
Monica Siikalampi <monica.siikalampi@icloud.com>
2024-12-04 18:44:30 +0200

    Added constants file for global Colors and Welcome screen

---
+++ app/constants/Colors.tsx
@@ -0,0 +1,10 @@
+const Colors = {
+    primaryBackground: "pink", // Päätaustaväri
+    secondaryBackground: "white", // Vaihtoehtoinen taustaväri
+    buttonBackground: "lightblue", // Napin taustaväri
+    buttonText: "black", // Napin tekstiväri
+    titleText: "black", // Otsikoiden väri
+    bodyText: "black", // Kuvauksen tai rungon tekstiväri
+};
+
+export default Colors;
