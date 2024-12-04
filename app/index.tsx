e0763883ef782e477c03cb105d4a52471fb51ff0
Monica Siikalampi <monica.siikalampi@icloud.com>
2024-12-04 22:22:57 +0200

    added overvirew and addexpenses screens to stack navigator

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,8 @@ import LoginScreen from './components/LoginScreen';
 import WelcomeScreen from './components/WelcomeScreen';
 import Colors from './constants/Colors';
 import RegisterScreen from './components/RegisterScreen';
+import OverviewScreen from './components/OverviewScreen';
+import AddExpenseIncomeScreen from './components/AddExpenseIncomeScreen';
 
 export default function Index() {
 
@@ -13,25 +15,33 @@ export default function Index() {
     <>
       <StatusBar style='light' />
       <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.buttonBackground },
-            headerTintColor: Colors.primaryBackground,
-            contentStyle: { backgroundColor: Colors.primaryBackground }
-          }}
-          initialRouteName='welcome'>
+        screenOptions={{
+          headerStyle: { backgroundColor: Colors.buttonBackground },
+          headerTintColor: Colors.primaryBackground,
+          contentStyle: { backgroundColor: Colors.primaryBackground }
+        }}
+        initialRouteName='welcome'>
+        <Stack.Screen
+          name='login'
+          component={LoginScreen}
+          options={{ title: 'Login' }} />
+        <Stack.Screen
+          name='welcome'
+          component={WelcomeScreen}
+          options={{ title: 'Welcome!' }} />
+        <Stack.Screen
+          name='register'
+          component={RegisterScreen}
+          options={{ title: 'Register' }} />
+        <Stack.Screen
+          name='overview'
+          component={OverviewScreen} 
+          options={{ title: 'Overview' }} /> 
           <Stack.Screen
-            name='login'
-            component={LoginScreen}
-            options={{ title: 'Login' }} />
-            <Stack.Screen
-            name='welcome'
-            component={WelcomeScreen}
-            options={{ title: 'Welcome!' }} />
-            <Stack.Screen
-            name='register'
-            component={RegisterScreen}
-            options={{ title: 'Register' }} />
-        </Stack.Navigator>
+          name='addExpense'
+          component={AddExpenseIncomeScreen} 
+          options={{ title: 'Add Expense' }} /> 
+      </Stack.Navigator>
     </>
 
   );
